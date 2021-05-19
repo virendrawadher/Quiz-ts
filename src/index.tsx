6bb565b50bfbd19cdbf75c41b46ba36e444f8b4b
virendrawadher <wadher32@gmail.com>
2021-05-19 20:30:07 +0530

    add: quiz context and router

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
+import { QuizProvider }from "./reducer/quiz-reducer"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <QuizProvider>
+        <App />
+      </QuizProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
