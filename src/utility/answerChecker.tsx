0c45521d9722f5a8e559cc687e4b179b9145ae13
Virendra Wadher <wadher32@gmail.com>
2021-06-09 22:54:06 +0530

    add: classes of material ui

--- src/utility/answerChecker.tsx
+++ src/utility/answerChecker.tsx
@@ -1,12 +1,17 @@
 import { Options } from '../type-aliases/type';
+import { ClassNameMap } from '@material-ui/styles';
 
-const answerChecker = (option: Options, selected: Options) => {
+const answerChecker = (
+	option: Options,
+	selected: Options,
+	classes: ClassNameMap,
+) => {
 	if (selected.isRight && selected.answer === option.answer) {
-		return 'select';
+		return classes.select;
 	} else if (selected.isRight === false && selected.answer === option.answer) {
-		return 'wrong';
+		return classes.wrong;
 	} else if (selected.isRight === false && option.isRight) {
-		return 'select';
+		return classes.select;
 	}
 };
 
