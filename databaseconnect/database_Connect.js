15a8be0b941a87994eebc99f5cfeea9e3e26d11f
Virendra Wadher <wadher32@gmail.com>
2021-06-09 23:03:55 +0530

    DB connection

---
+++ databaseconnect/database_Connect.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose
+	.connect(process.env.DB_URI, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useCreateIndex: true,
+	})
+	.then(() => console.log('database connected'))
+	.catch((error) => console.log('error while connecting db', error));
